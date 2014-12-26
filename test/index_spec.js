82c0ee2986ae7293b9d00bc07eccdaf007c98254
korve <aeckardt@outlook.com>
2014-12-26 20:35:40 +0100

    added testing framework skeleton

---
+++ test/index_spec.js
@@ -0,0 +1,12 @@
+(function() {
+	"use strict";
+
+	var chai = require("chai"),
+		expect = chai.expect;
+
+	describe("Test framework tests", function(){
+		it("Should run tests",  function() {
+			expect(true).to.be.true;
+		});
+	});	
+})();
